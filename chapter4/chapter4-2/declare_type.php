169f26a061eb436f052a95cb2d650f52754970cc
kingkazuma7 <nasionasio7@gmail.com>
2018-10-18 08:56:57 +0900

    4-2 userfunction 181018

---
+++ chapter4/chapter4-2/declare_type.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>引数int型として受け取る</title>
+</head>
+<body>
+  <pre>
+  <?php
+  function twice(int $var) {
+    $var *= 2;
+    return $var;
+  }
+  $num = 10.8;
+  $result = twice($num);
+  echo "{$num}の２倍は", $result, "<br>";
+  ?>
+  </pre>
+</body>
+</html>
