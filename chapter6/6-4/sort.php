25285286de426495a3d69f1aedb5a8862184e6b3
kingkazuma7 <nasionasio7@gmail.com>
2018-11-03 23:00:52 +0900

    6-4 ソート 181103

---
+++ chapter6/6-4/sort.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>配列を昇順にソートする</title>
+</head>
+<body>
+<pre>
+<?php
+$data = [23, 16, 8, 42, 15, 4];
+sort($data);
+print_r($data);
+?>
+</pre>
+</body>
+</html>
