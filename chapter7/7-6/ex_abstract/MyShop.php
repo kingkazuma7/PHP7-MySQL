da91c671738ba950f409d3ac95e0c07c3b7e578a
kingkazuma7 <nasionasio7@gmail.com>
2018-11-09 08:48:10 +0900

    7-6 抽象クラス 181109
    Signed-off-by: kingkazuma7 <nasionasio7@gmail.com>

---
+++ chapter7/7-6/ex_abstract/MyShop.php
@@ -0,0 +1,20 @@
+<?php
+require_once("ShopBiz.php");
+
+class MyShop extends ShopBiz {
+  // ShopBiz抽象クラスで指定されているメソッド
+  public function thanks(){
+    echo "ありがとうございました！", "\n";
+  }
+
+  // 販売する
+  public function hanbai($tanka, $kosu){
+    $price = $tanka * $kosu;
+    $this->sell($price);
+  }
+  // 売上合計を調べる
+  public function getUriage(){
+    echo "売上合計は、{$this->uriage}円です。";
+  }
+}
+// ?>
